b09d029f2c9cf4f5f413fabd43db016c1ca3ceff
Nurlan Kerimov <34273025+kerimoff@users.noreply.github.com>
2021-04-07 20:58:21 +0300

    added Figure 4A

--- manuscript/figures/eqtl_forestplot_and_factor_loadings/eqtl_forestplot.R
+++ manuscript/figures/eqtl_forestplot_and_factor_loadings/eqtl_forestplot.R
@@ -4,31 +4,30 @@ library(data.table)
 
 ontology_map = readr::read_tsv("../../../ontology_mappings/tissue_ontology_mapping.tsv")
 friendly_names = readr::read_tsv("../../../ontology_mappings/friendly_names.tsv") %>%
-  dplyr::select(ontology_term, ontology_tissue)
-# rename blueprint dataset for consistency with effect dataset names
+  dplyr::select(tissue_ontology_id, tissue_label)
 ontology_map$study[c(1,2,3)] = c("BLUEPRINT_SE", "BLUEPRINT_SE", "BLUEPRINT_PE")
 ontology_map <- ontology_map %>% dplyr::mutate(study_qtlgroup = paste0(study, ".", qtl_group)) %>%
   dplyr::left_join(friendly_names) %>%
-  dplyr::mutate(sample_class = ifelse(ontology_tissue %like% "brain", "brain", "other")) %>%
-  dplyr::mutate(sample_class = ifelse(ontology_tissue %like% "LCL", "LCL", sample_class)) %>%
-  dplyr::mutate(sample_class = ifelse(ontology_tissue %like% "monocyte", "monocyte", sample_class)) %>%
-  dplyr::mutate(sample_class = ifelse(ontology_tissue %like% "macrophage", "macrophage", sample_class)) %>%
-  dplyr::mutate(sample_class = ifelse(ontology_tissue %like% "blood", "blood", sample_class)) %>%
-  dplyr::mutate(sample_class = ifelse(ontology_tissue %like% "neutrophil", "neutrophil", sample_class)) %>%
-  dplyr::mutate(sample_class = ifelse(ontology_term %in% c("CL_0000236","CL_0002677","CL_0002678","CL_0000624","CL_0000625","CL_0000623","CL_0000899","CL_0000546","CL_0000545","CL_0000899","CL_0002038","CL_0000084"), "lymphocyte", sample_class)) %>%
-  dplyr::mutate(sample_class = ifelse(ontology_tissue %like% "iPSC", "iPSC", sample_class))
+  dplyr::mutate(sample_class = ifelse(tissue_label %like% "brain", "brain", "other")) %>%
+  dplyr::mutate(sample_class = ifelse(tissue_label %like% "LCL", "LCL", sample_class)) %>%
+  dplyr::mutate(sample_class = ifelse(tissue_label %like% "monocyte", "monocyte", sample_class)) %>%
+  dplyr::mutate(sample_class = ifelse(tissue_label %like% "macrophage", "macrophage", sample_class)) %>%
+  dplyr::mutate(sample_class = ifelse(tissue_label %like% "blood", "blood", sample_class)) %>%
+  dplyr::mutate(sample_class = ifelse(tissue_label %like% "neutrophil", "neutrophil", sample_class)) %>%
+  dplyr::mutate(sample_class = ifelse(tissue_ontology_id %in% c("CL_0000236","CL_0002677","CL_0002678","CL_0000624","CL_0000625","CL_0000623","CL_0000899","CL_0000546","CL_0000545","CL_0000899","CL_0002038","CL_0000084"), "lymphocyte", sample_class)) %>%
+  dplyr::mutate(sample_class = ifelse(tissue_label %like% "iPSC", "iPSC", sample_class))
 
 fct_levels = c("blood","lymphocyte","LCL","neutrophil","monocyte","macrophage","brain","iPSC","other")
 ontology_map = dplyr::mutate(ontology_map, tissue_fct = factor(sample_class, levels = fct_levels))
 # place immune t-cells together
-ontology_map = ontology_map %>% dplyr::mutate(dummy = ifelse(qtl_group %like% "anti", "ANTI", ontology_tissue), 
-                                              ontology_tissue = ifelse(qtl_group %like% "anti", paste(ontology_tissue, "(anti-CD3-CD28)"), ontology_tissue))
+ontology_map = ontology_map %>% dplyr::mutate(dummy = ifelse(qtl_group %like% "anti", "ANTI", tissue_label), 
+                                              tissue_label = ifelse(qtl_group %like% "anti", paste(tissue_label, "(anti-CD3-CD28)"), tissue_label))
 # place quach monocyte and nedelec macrophage together because they load on one factor
 ontology_map = ontology_map %>% dplyr::mutate(dummy = ifelse(study_qtlgroup == "Quach_2016.monocyte_naive", "z-monocyte", dummy))
 ontology_map = ontology_map %>% dplyr::mutate(dummy = ifelse(study_qtlgroup == "Nedelec_2016.macrophage_naive", "a-macrophage", dummy))
 # sort studies
 ontology_map = ontology_map %>% dplyr::arrange(tissue_fct, dummy, study)
-ontology_map = ontology_map %>% dplyr::mutate(heatmap_label = paste(study, ontology_tissue, sep=" "))
+ontology_map = ontology_map %>% dplyr::mutate(heatmap_label = paste(study, tissue_label, sep=" "))
 
 # remove stimulated datasets
 datasets = ontology_map$study_qtlgroup
@@ -43,7 +42,7 @@ ontology_map = ontology_map[datasets,]
 
 # read effects data for the RBMS1 gene
 effects = readr::read_tsv("rbms1_effects.tsv")
-eqtl = effects %>% dplyr::filter(eqtl_id == "chr2_160468964_A_T.ENSG00000153250")
+eqtl = effects %>% dplyr::filter(eqtl_id == "chr2_160430631_G_A.ENSG00000153250")
 
 # function to extract beta, se and replace NA if needed
 effects_to_matricies = function(effects, replace_na_with = FALSE){
@@ -71,7 +70,9 @@ effect = tibble(beta = data$beta[1,], qtl_group = names(data$beta[1,]), se=data$
 # calculate 95% confidence interval
 conf.level = 0.95
 ci.value <- -qnorm( ( 1 - conf.level ) / 2 )
-effect = mutate(effect, interval = ci.value * se)
+effect = mutate(effect, interval = ci.value * se) %>% 
+  mutate(qtl_group = gsub(pattern = "GTExV8", replacement = "GTEx", x = qtl_group))
+
 
 colors = c("#e41a1c","#377eb8","#4daf4a","#984ea3","#ff7f00","#a65628","#fed976","#f781bf","#999999")
 names(colors) = c("blood","lymphocyte","LCL","neutrophil","monocyte","macrophage","brain","iPSC","other")
